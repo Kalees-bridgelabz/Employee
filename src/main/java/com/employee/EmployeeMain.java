9905162f716efd11a18f14ba123bb5a6c057d09b
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-03-14 12:57:20 +0530

    UC9: Ability to manage Employee

--- src/main/java/com/employee/EmployeeMain.java
+++ src/main/java/com/employee/EmployeeMain.java
@@ -3,14 +3,12 @@ package com.employee;
 import java.util.Scanner;
 
 /**
- * UC8 - Ability to save the Total
+ * UC9 - Ability to manage Employee
  *
  * Requirements:
-     *   - Wage for Each
-     *   - Company
-     *   - Note: You can Create
-     *   - EmpWageBuilder for each
-     *   - Company
+     *   - Wage of multiple
+     *   - companies
+     *   - Note:
  *
  * @author  Kalees-bridgelabz
  * @version 1.0
@@ -20,10 +18,10 @@ public class EmployeeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee");
         
-        System.out.println("\n=== UC8: Ability to save the Total ===");
-        // TODO: Implement UC8
+        System.out.println("\n=== UC9: Ability to manage Employee ===");
+        // TODO: Implement UC9
         // 
-Wage for Each        // Company        // Note: You can Create
-        System.out.println("UC8 - Implementation pending");
+Wage of multiple        // companies        // Note:
+        System.out.println("UC9 - Implementation pending");
     }
 }
