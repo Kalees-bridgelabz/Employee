2554207f94c4df4a8b2adb0a69c83a7fa7921248
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-03-14 12:57:25 +0530

    UC10: CompanyEmpWage Object

--- src/main/java/com/employee/EmployeeMain.java
+++ src/main/java/com/employee/EmployeeMain.java
@@ -3,11 +3,13 @@ package com.employee;
 import java.util.Scanner;
 
 /**
- * UC9 - Ability to manage Employee
+ * UC10 - CompanyEmpWage Object
  *
  * Requirements:
-     *   - Wage of multiple
-     *   - companies
+     *   - Ability to manage
+     *   - Employee Wage of
+     *   - multiple companies using
+     *   - Interface approach
      *   - Note:
  *
  * @author  Kalees-bridgelabz
@@ -18,10 +20,24 @@ public class EmployeeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee");
         
-        System.out.println("\n=== UC9: Ability to manage Employee ===");
-        // TODO: Implement UC9
-        // 
-Wage of multiple        // companies        // Note:
-        System.out.println("UC9 - Implementation pending");
+        System.out.println("\n=== UC10: CompanyEmpWage Object ===");
+        final int WAGE_PER_HOUR   = 20;
+        final int FULL_DAY_HOURS  = 8;
+        final int PART_TIME_HOURS = 4;
+        final int WORKING_DAYS    = 20;
+        final int MAX_HOURS       = 100;
+
+        int totalWage = 0, totalHours = 0, day = 0;
+        java.util.Random rand = new java.util.Random();
+
+        while (totalHours < MAX_HOURS && day < WORKING_DAYS) {
+            int attendance = rand.nextInt(3); // 0=absent,1=full,2=part
+            int hoursWorked = (attendance == 1) ? FULL_DAY_HOURS
+                            : (attendance == 2) ? PART_TIME_HOURS : 0;
+            totalWage  += hoursWorked * WAGE_PER_HOUR;
+            totalHours += hoursWorked;
+            day++;
+        }
+        System.out.println("Total Wage: " + totalWage + " | Days: " + day + " | Hours: " + totalHours);
     }
 }
